e599bdaf0133653012a87c6df8ceca740b226bed
watcharaphat <watcharaphat.3166@gmail.com>
2018-06-04 09:07:12 +0700

    add api to join and list ip

--- src/server.js
+++ src/server.js
@@ -1,1 +1,28 @@
-console.log('hello');
+import express from 'express';
+
+const app = express();
+const router = express.Router();
+
+const ips = [];
+
+router.get('/join/:ip', (req, res, err) => {
+  const ip = req.params.ip;
+  let result = false;
+
+  if (ips.indexOf(ip) === -1) {
+    ips.push(ip);
+    result = true;
+  }
+
+  res.json({result});
+});
+
+router.get('/ip', (req, res, err) => {
+  res.json({ip: ips});
+});
+
+app.use(router);
+
+console.log('server is listening at port 3000');
+app.listen(3000);
+
